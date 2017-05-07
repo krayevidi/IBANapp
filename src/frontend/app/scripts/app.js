c0b92b2f3accd70c46c2ec9a19e5cb309f9edd1b
Andrew Sutulov <andrew.sutulov@gmail.com>
2017-05-07 22:08:51 +0300

    [frontend] added initial project layout

---
+++ src/frontend/app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontendApp
+ * @description
+ * # frontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontendApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function () {});
